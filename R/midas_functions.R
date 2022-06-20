08c1cc54591bc95d2f76c23da82c5622d59d14bf
Tom Robinson <ts.robinson1994@gmail.com>
2022-06-20 21:47:13 +0100

    add headless mode 'agg'

--- R/midas_functions.R
+++ R/midas_functions.R
@@ -358,7 +358,7 @@ overimpute <- function(# Input data
   if (inherits(matplot_render, "try-error")) {
     stop("Cannot load TkAgg, which is needed to render the overimputation plot.\n You can try installing TkAgg by running the following at the command line: `sudo apt-get install python3-tk' ")
   } else {
-    matplot_render <- try(matplotlib$use("agg"), silent = TRUE)
+    matplot_render <- try(matplotlib$use("Agg"), silent = TRUE)
     if (inherits(matplot_render, "try-error")) {
       stop("Could not load TkAgg. Tried to run in headless mode, but failed.")
     }
